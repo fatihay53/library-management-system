4ae77ef2717e2adcb1552e8db2e386a5d3d92ea8
Fatih Ay <f.sultanay@gmail.com>
2021-03-01 15:35:04 -0500

    orm file updated

--- app/orm.js
+++ app/orm.js
@@ -3,18 +3,22 @@ const db = require('./connection')('library_managment_system', 'password123')
 
 
 // ====================fatih=============================
-// async function addCategory(input) {
-//     return db.query('INSERT INTO category (category_name,category_des) values (?)', [input.category_name, input.category_des])
-// }
 
-// async function deleteCategory(id) {
-//     return db.query(`DELETE FROM category WHERE id='${id}'`)
+//=================//CATEGORY============================
+async function addCategory(input) {
+    return db.query('INSERT INTO category (categoryName,categoryDes) values (?)', [input.categoryName, input.categoryDes])
+}
 
-// }
+async function deleteCategory(id) {
+    return db.query(`DELETE FROM category WHERE id='${id}'`)
 
-// async function updateCategory(id, input) {
-//     return db.query(`UPDATE category SET category_name = '${input.category_name}' AND category_des='${input.category_des}' WHERE id = ${id}`)
-// }
+}
+
+async function updateCategory(id, input) {
+    return db.query(`UPDATE category SET categoryName = '${input.categoryName}', categoryDes='${input.categoryDes}' WHERE id = ${id}`)
+}
+
+//==================================end==================================================
 
 // ===============================george==================================================
 // the pramater is a object {memberID, firstName, lastName, phoneNum, email, address}
@@ -23,12 +27,18 @@ async function addMember(data) {
         [data.firstName, data.lastName, data.phoneNumber, data.email, data.address])
     console.log(result)
 }
-
-
-
-
-
 // ================================end==================================================
 
-module.exports = { addMember }
+
+
+//===========================Books=======================================
+async function addBook(input) {
+    return db.query('INSERT INTO book (bookName,author,publishingYear,memberID,categoryID,barrowDate) values (?)',
+        [input.bookName, input.author, input.publishingYear, input.memberID, input.categoryID, input.barrowDate])
+}
+async function updateBook(input){
+    
+}
+
+module.exports = { addMember, addCategory, deleteCategory, updateCategory,addBook }
 
