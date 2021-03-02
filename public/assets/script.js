906d3a27854285baaa40b5b519993f6f7e359f23
Fatih Ay <f.sultanay@gmail.com>
2021-03-01 21:52:33 -0500

    add book and add category html file updated

--- public/assets/script.js
+++ public/assets/script.js
@@ -24,3 +24,50 @@
 //         // a rander function should be here
 //     )
 // }
+
+// fetch url from server and get respond
+
+//  get api for adding book
+async function getBookInfo() {
+    const data = await fetch('api/addbook').then(r => r.json())
+
+    document.querySelector('#bookList').innerHTML = ''
+    data.forEach(input => {
+        document.querySelector('#bookList').innerHTML += `
+    
+    ${input.bookName}
+    ${input.author}
+    ${input.publishingYear}
+    ${input.memberID}
+    ${input.categoryID}
+    ${input.barrowDate}
+    // CARD will be past here 
+
+    `
+
+    })
+}
+getBookInfo()
+
+async function editBook() {
+    const editData = {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'get',
+
+    }
+    const data = await fetch(`api/updatebook/${id}`).then(r => r.json())
+    getBookInfo()
+
+    document.querySelectora('#').value = data[0].bookName
+    document.querySelectora('#').value = data[0].author
+    document.querySelectora('#').value = data[0].publishingYear
+    document.querySelectora('#').value = data[0].memberID
+    document.querySelectora('#').value = data[0].categoryID
+    document.querySelectora('#').value = data[0].barrowDate
+
+
+}
+
+
+
+
