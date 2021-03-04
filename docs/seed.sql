27091e62ed2d231c4395ce672036762fdb557e68
ilias <iliasc8@gmail.com>
2021-03-04 11:28:57 -0500

    updated seeds.sql

--- docs/seed.sql
+++ docs/seed.sql
@@ -1,6 +1,1 @@
-insert into category (categoryName, categoryDes) values ("Business", "Business books etc..");
-
-
-insert into category (categoryName, categoryDes) values ("Science", "Science books etc..");
-
 
